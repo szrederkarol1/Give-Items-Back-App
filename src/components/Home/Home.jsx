ebc7f262446125847a441bc97407c6d96d690924
Karol <szrederkarol1@gmail.com>
2023-07-05 22:38:05 +0200

    Complete section Contact

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,9 @@
 import Home_header from "./Home_header";
 import Home_columns from "./Home_columns";
 import Steps from "./Home_steps";
-import AboutUs from "./About_us";
+import AboutUs from "./Home_about_us";
 import Cards from "./Home_cards";
+import Contact from "./Home_contact";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <Steps />
       <AboutUs />
       <Cards />
+      <Contact />
     </>
   );
 };
---
+++ src/components/Home/Home_contact.jsx
@@ -0,0 +1,39 @@
+import "../../scss/settings/Home/home_contact.scss";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="contact_container">
+        <div className="container_right">
+          <div className="container_title">
+            <p className="title">Skontaktuj się z nami</p>
+            <div className="ornament"></div>
+          </div>
+          <div className="form_container">
+            <div className="top_form">
+              <div className="form_name">
+                <p style={{ marginBottom: "12px" }}>Wpisz swoje imię</p>
+                <input type="text" placeholder="Krzysztof"></input>
+              </div>
+              <div className="form_email">
+                <p style={{ marginBottom: "12px" }}>Wpisz swój email</p>
+                <input type="text" placeholder="xyz123@me.com"></input>
+              </div>
+            </div>
+            <div className="form_bottom">
+              <p style={{ marginBottom: "12px" }}>Wpisz swoją wiadomość</p>
+              <textarea placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."></textarea>
+            </div>
+            <button className="send_message">Wyślij</button>
+          </div>
+        </div>
+        <div className="copyright_icons">
+          <div className="signature"></div>
+          <div className="icons"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
