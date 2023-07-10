3633126e513b95114e51d76e919686d7581b42b8
Karol <szrederkarol1@gmail.com>
2023-07-10 23:56:30 +0200

    add validation form

--- src/components/Home/Home_contact.jsx
+++ src/components/Home/Home_contact.jsx
@@ -1,43 +1,93 @@
 import "../../scss/settings/Home/home_contact.scss";
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    const newErrors = {};
+
+    if (name.trim() === '') {
+      newErrors.name = 'Proszę wpisać swoje imię.';
+    }
+
+    if (email.trim() === '') {
+      newErrors.email = 'Proszę wpisać swój email.';
+    } else if (!isValidEmail(email)) {
+      newErrors.email = 'Proszę wpisać poprawny adres email.';
+    }
+
+    if (message.trim() === '') {
+      newErrors.message = 'Proszę wpisać swoją wiadomość.';
+    }
+
+    setErrors(newErrors);
+
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const isValidEmail = (email) => {
+    // Prosta walidacja adresu email (możesz użyć bardziej zaawansowanej walidacji)
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validateForm();
+
+    if (isValid) {
+      // Wykonaj akcję, np. wysyłkę formularza
+      console.log('Formularz jest poprawny');
+      console.log('Imię:', name);
+      console.log('Email:', email);
+      console.log('Wiadomość:', message);
+
+      // Resetuj pola formularza
+      setName('');
+      setEmail('');
+      setMessage('');
+      setErrors({});
+    }
+  };
+
   return (
-    <>
-      <a name="Contact"></a>
-      <div className="contact_container">
-        <div className="container_right">
-          <div className="container_title">
-            <p className="title">Skontaktuj się z nami</p>
-            <div className="ornament"></div>
-          </div>
-          <div className="form_container">
-            <div className="top_form">
-              <div className="form_name">
-                <p style={{ marginBottom: "12px" }}>Wpisz swoje imię</p>
-                <input type="text" placeholder="Krzysztof"></input>
-              </div>
-              <div className="form_email">
-                <p style={{ marginBottom: "12px" }}>Wpisz swój email</p>
-                <input type="text" placeholder="xyz123@me.com"></input>
-              </div>
-            </div>
-            <div className="form_bottom">
-              <p style={{ marginBottom: "12px" }}>Wpisz swoją wiadomość</p>
-              <textarea placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."></textarea>
-            </div>
-            <button type="submit" className="send_message">
-              Wyślij
-            </button>
-          </div>
-        </div>
-        <div className="copyright_icons">
-          <div className="signature"></div>
-          <div className="icons"></div>
-        </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Imię:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        {errors.name && <span className="error">{errors.name}</span>}
       </div>
-    </>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {errors.email && <span className="error">{errors.email}</span>}
+      </div>
+      <div>
+        <label htmlFor="message">Wiadomość:</label>
+        <textarea
+          id="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        {errors.message && <span className="error">{errors.message}</span>}
+      </div>
+      <button type="submit">Wyślij</button>
+    </form>
   );
-};
+}
 
 export default Contact;
