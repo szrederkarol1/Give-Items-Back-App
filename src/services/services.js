2b892571c24bdb297ca71ff97ee997f716c167eb
Karol <szrederkarol1@gmail.com>
2023-07-23 21:04:08 +0200

    Code register

--- src/services/services.js
+++ src/services/services.js
@@ -4,4 +4,5 @@ const supabase = createClient(
   "https://fiqxmkykdwiqlbdvcqzf.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpcXhta3lrZHdpcWxiZHZjcXpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAwNjA0NDksImV4cCI6MjAwNTYzNjQ0OX0.8iSHtK9O_4z76OSyEGwIwxlMJ7Y1GsNv875ji-8mpa8"
 );
+
 export default supabase;
